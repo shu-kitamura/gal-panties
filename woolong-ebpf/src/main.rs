f6be133f4ab26aa7e5e88211decf7e47d830e1c7
shu-kitamura <shusei3316@yahoo.co.jp>
2025-09-05 22:42:29 +0900

    add woolong

---
+++ woolong-ebpf/src/main.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
+use aya_log_ebpf::info;
+
+#[xdp]
+pub fn woolong(ctx: XdpContext) -> u32 {
+    info!(&ctx, "woolong called");
+    match try_woolong(ctx) {
+        Ok(ret) => ret,
+        Err(_) => xdp_action::XDP_ABORTED,
+    }
+}
+
+fn try_woolong(ctx: XdpContext) -> Result<u32, u32> {
+    info!(&ctx, "received a packet");
+    Ok(xdp_action::XDP_PASS)
+}
+
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[unsafe(link_section = "license")]
+#[unsafe(no_mangle)]
+static LICENSE: [u8; 13] = *b"Dual MIT/GPL\0";
